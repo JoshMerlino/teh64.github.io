b0c40d08566f60daebcaf4f88a49df20d87c67ff
TehPicix <teh.picix@gmail.com>
2017-01-23 21:05:23 -0500

    Add files via upload

---
+++ material-design-wiki/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	
+	$(".material-icons.menu").click(function(e){
+		e.stopPropagation();
+		$(".banner.sidebar").animate({
+			left: "0px"
+		}, 200).noclick(function(){
+			$(".banner.sidebar").animate({
+				left: "-360px"
+			}, 200)
+		})
+	})
+	$(".navmenu.option").click(function(){
+		$(".navmenu.option").removeClass("sel");
+		$(this).addClass("sel")
+		
+		var t = $(this).offset().top - 172
+		
+		$(this).animate({
+			color:"#FFF"
+		}, 200).siblings().not(".tit").animate({
+			color: "#000"
+		}, 200)
+		
+		$(".navmenu.option.back").animate({
+			top:t
+		}, 200);
+		var cn = $(this).attr("class").split(" ")[2]
+		$(".content").css({
+			display: "none"
+		})
+		$(".content." + cn).css({
+			display: "block" 
+		})
+		$(".content-name").text($(this).text().toProperCase())
+	})
+});
