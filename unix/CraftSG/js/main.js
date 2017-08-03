13c6ae9493979901d0255fb0abcce2b4d60a1096
TehPicix <teh.picix@gmail.com>
2017-08-03 09:22:26 -0400

    Add files via upload

--- unix/CraftSG/js/main.js
+++ unix/CraftSG/js/main.js
@@ -1,17 +1,18 @@
-var ping = {}
+var ping = {};
 
 $(function(){
 	
+	$("[ping]").each(function(){
+		var d = 58//$(this).height();
+		$(this).html('<svg class="spinner" width="'+d+'px" height="'+d+'px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg>');
+	})
+	
 	$.getJSON("https://api.minetools.eu/ping/psg.biz.tm/25565", function(json){
         ping = json;
-		console.log(ping);
 		$("*").filter("[ping]").each(function(){
-			var a = $(this).attr("ping")
-			$(this).html(eval("ping." + a))
-		})
-		
-		$(".wrapper").remove();
-		
+			var a = $(this).attr("ping");
+			$(this).html(eval("ping." + a));
+		});
     });
 	
 	$(".navbar .menu").click(function(){
